e84abd750bf3975adaecb62e7ed1ec1cedb90d81
Joshua Nixon <3789xs@gmail.com>
2021-10-04 23:38:19 -0500

    Fixed .env loading problems, added !hello function, cleaned up code a bit.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import discord
+import os
 
+# Tricky env import for VSC
+from dotenv import load_dotenv
+
+# start discord client
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('{0.user} has logged into the matrix.'.format(client))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.lower().trim() == "!hello":
-        await message.channel.send('Hello!')
+    if message.content.lower() == "!hello":
+        await message.channel.send('Hello {}!'.format(message.author.name))
 
-client.run()
+load_dotenv('secrets.env')
+token = os.getenv('TOKEN')
+client.run(token)
